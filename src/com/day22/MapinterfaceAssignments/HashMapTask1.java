6dd9e30fdc728ff1bd4c90643a30d63e0738f956
Sunki Reddy Jahnavi <98159788+jahavi@users.noreply.github.com>
2025-12-30 07:33:21 -0600

    Add files via upload

---
+++ src/com/day22/MapinterfaceAssignments/HashMapTask1.java
@@ -0,0 +1,11 @@
+package com.day22.MapinterfaceAssignments;
+import java.util.*;
+public class HashMapTask1 {
+    public static void main(String[] args){
+        HashMap<String,String> map=new HashMap<>();
+        map.put("A","Apple");
+        map.put("B","Banana");
+        map.put("C","Cat");
+        System.out.println("HashMap Task 1 executed: "+map);
+    }
+}
---
+++ src/com/day22/MapinterfaceAssignments/HashMapTask11.java
@@ -0,0 +1,11 @@
+package com.day22.MapinterfaceAssignments;
+import java.util.*;
+public class HashMapTask11 {
+    public static void main(String[] args){
+        HashMap<String,String> map=new HashMap<>();
+        map.put("A","Apple");
+        map.put("B","Banana");
+        map.put("C","Cat");
+        System.out.println("HashMap Task 11 executed: "+map);
+    }
+}
